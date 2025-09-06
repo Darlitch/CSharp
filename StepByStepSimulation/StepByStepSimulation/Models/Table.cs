ad822da88b278c56afa8a5ce4adcbb6b8f00151e
Darlitch <errorguy@mail.ru>
2025-09-06 21:06:00 +0700

    ver 0.1: Models Done

---
+++ StepByStepSimulation/StepByStepSimulation/Models/Table.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using StepByStepSimulation.Enums;
+
+namespace StepByStepSimulation.Models
+{
+    public class Table
+    {   
+        private const int _philosopherCount = 5;
+        public List<ForkState> Forks { get; set; }
+        public List<Philosopher> Philosophers { get; set; }
+
+        public Table()
+        {
+            Forks = Enumerable.Repeat(ForkState.Available, 5).ToList();
+            Philosophers = PhilosopherInitializer.InitPhilosophers();
+        }
+    }
+}
