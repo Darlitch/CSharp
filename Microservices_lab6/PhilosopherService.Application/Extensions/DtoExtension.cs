8c076c412a04886e3f81f5bf99fa5cdf60f16c70
Darlitch <errorguy@mail.ru>
2025-11-27 21:48:59 +0700

    ver 0.5 Create Strategy, DtoExtensions and Options

---
+++ Microservices_lab6/PhilosopherService.Application/Extensions/DtoExtension.cs
@@ -0,0 +1,11 @@
+﻿using Contract.Dtos;
+using PhilosopherService.Domain;
+
+namespace PhilosopherService.Application.Extensions;
+
+public static class DtoExtension
+{
+    public static PhilosopherMetricsDto ToPhilosopherMetricsDto(this PhilosopherMetrics metrics)
+        => new PhilosopherMetricsDto(metrics.State, metrics.Action, metrics.CurrentActionDuration, metrics.Eaten,
+            metrics.WaitingTime);
+}
---
+++ Microservices_lab6/PhilosopherService.Application/Services/NaiveStrategy.cs
@@ -0,0 +1,21 @@
+﻿using Contract.Enums;
+using PhilosopherService.Application.Abstractions;
+using PhilosopherService.Domain.Enums;
+
+namespace PhilosopherService.Application.Services;
+
+public class NaiveStrategy : IStrategy
+{
+    public PhilosopherAction SelectAction(ForkState leftFork, ForkState rightFork)
+    {
+        if ((leftFork == ForkState.NotInHand))
+        {
+            return PhilosopherAction.TakeLeftFork;
+        }
+        if ((leftFork == ForkState.InHand) && rightFork == ForkState.NotInHand)
+        {
+            return PhilosopherAction.TakeRightFork;
+        }
+        return PhilosopherAction.None;
+    }
+}
